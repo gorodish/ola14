3879cda654f05fd6ba5a6e5bddc262e786f8a992
mcquam <murray.mcquarrie@gmail.com>
2019-11-08 07:04:52 +0000

    back to new files

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Button } from 'semantic-ui-react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Ola14</h1>
+        <Button icon="smile" content="React button" />
+      </div>
+    )
+  }
+}
+
